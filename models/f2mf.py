2d4d721e0bf024f76383c3b47df82749182c4b6e
jrukavina <jakov.rukavina96@gmail.com>
2023-04-25 22:49:35 +0200

    added base F2MF implementation

---
+++ models/f2mf.py
@@ -0,0 +1,365 @@
+import torch
+from torchvision.ops import DeformConv2d
+import torch.nn.functional as F
+from spatial_correlation_sampler import SpatialCorrelationSampler
+
+
+def warp(x, flo):
+    """
+    warp an image/tensor (im2) back to im1, according to the optical flow
+    x: [B, C, H, W] (im2)
+    flo: [B, 2, H, W] flow
+    """
+    B, C, H, W = x.size()
+    # mesh grid
+    xx = torch.arange(0, W).view(1, -1).repeat(H, 1)
+    yy = torch.arange(0, H).view(-1, 1).repeat(1, W)
+    xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    grid = torch.cat((xx, yy), 1).float()
+
+    if x.is_cuda:
+        grid = grid.cuda()
+    vgrid = grid + flo
+
+    # scale grid to [-1,1]
+    vgrid[:, 0, :, :] = 2.0 * vgrid[:, 0, :, :].clone() / max(W - 1, 1) - 1.0
+    vgrid[:, 1, :, :] = 2.0 * vgrid[:, 1, :, :].clone() / max(H - 1, 1) - 1.0
+
+    vgrid = vgrid.permute(0, 2, 3, 1)
+    output = F.grid_sample(x, vgrid, align_corners=True)
+    # mask = torch.ones(x.size()).cuda()
+    # mask = F.grid_sample(mask, vgrid, align_corners=True)
+    #
+    # mask[mask < 0.999] = 0
+    # mask[mask > 0] = 1
+    #
+    # return output * mask
+    return output
+
+
+def normalize(x, mean, std):
+    return (x - mean) / std
+
+
+def normalize_concatenated(x, mean, std):
+    base_c = mean.shape[1]
+    for i in range(4):
+        x[:, i * base_c:(i + 1) * base_c, :, :] -= mean
+        x[:, i * base_c:(i + 1) * base_c, :, :] /= std
+    return x
+
+
+def unnormalize(x, mean, std):
+    return (x * std) + mean
+
+
+class BN_ReLU_DConv(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, offset_kernel_size=None, custom_offset_op=None):
+        super(BN_ReLU_DConv, self).__init__()
+
+        if offset_kernel_size is None:
+            offset_kernel_size = kernel_size
+
+        self.bn = torch.nn.BatchNorm2d(in_channels)
+
+        if custom_offset_op is not None:
+            self.offset_conv = custom_offset_op
+        else:
+            self.offset_conv = torch.nn.Conv2d(in_channels=in_channels,
+                                               out_channels=2 * kernel_size * kernel_size,
+                                               kernel_size=offset_kernel_size,
+                                               stride=1,
+                                               padding=int((offset_kernel_size - 1) / 2),
+                                               bias=True)
+
+        self.dconv = DeformConv2d(in_channels=in_channels,
+                                  out_channels=out_channels,
+                                  kernel_size=kernel_size,
+                                  stride=1,
+                                  padding=int((kernel_size - 1) / 2),
+                                  bias=True)
+
+    def forward(self, x):
+        x = torch.relu(self.bn(x))
+        offsets = self.offset_conv(x)
+        x = self.dconv(x, offsets)
+        return x
+
+
+class BN_ReLU_Conv(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(BN_ReLU_Conv, self).__init__()
+
+        self.bn = torch.nn.BatchNorm2d(in_channels)
+
+        self.conv = torch.nn.Conv2d(in_channels=in_channels,
+                                    out_channels=out_channels,
+                                    kernel_size=kernel_size,
+                                    stride=1,
+                                    padding=int((kernel_size - 1) / 2),
+                                    bias=False)
+
+    def forward(self, x):
+        x = torch.relu(self.bn(x))
+        x = self.conv(x)
+        return x
+
+
+class CorrelationModule(torch.nn.Module):
+    def __init__(self, in_channels_one, out_channels_one=128, patch_size=9, kernel_size=1):
+        super(CorrelationModule, self).__init__()
+
+        self.in_channels_one = in_channels_one
+        self.out_channels_one = out_channels_one
+        self.patch_size = patch_size
+
+        self.bn_relu_conv = BN_ReLU_Conv(in_channels_one, out_channels_one, 3)
+        self.corr_layer = SpatialCorrelationSampler(
+            kernel_size=kernel_size,
+            patch_size=patch_size,
+            stride=1,
+            padding=0,
+            dilation=1,
+            dilation_patch=2)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        feats = [x[:, self.in_channels_one * i: self.in_channels_one * (i + 1), :, :] for i in range(4)]  # split by T
+        conv_feats = [self.bn_relu_conv(x) for x in feats]
+        norm_feats = [x / x.norm(dim=1, keepdim=True) for x in conv_feats]
+
+        corr_list = []
+        for i in range(4 - 1):
+            corr_list.append(self.corr_layer(norm_feats[i], norm_feats[i + 1]).reshape(B, self.patch_size ** 2, H, W))
+
+        corr_feats = torch.cat(corr_list, dim=1)
+
+        return corr_feats
+
+
+class F2MF(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, mean=None, std=None):
+        super(F2MF, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.mean, self.std = None, None
+
+        self.do_normalization = mean is not None and std is not None
+        if self.do_normalization:
+            self.mean = mean.reshape((1, mean.shape[0], 1, 1))
+            self.std = std.reshape((1, std.shape[0], 1, 1))
+
+        patch_size = 9
+        input_frames = 4
+        self.correlation = CorrelationModule(in_channels_one=out_channels, out_channels_one=128,
+                                             patch_size=patch_size)
+        self.fusion = BN_ReLU_DConv(in_channels, out_channels, 1)
+        in_channels_with_corr = out_channels + patch_size ** 2 * (input_frames - 1)
+
+        # shared dconv
+        shared_dconv_layers = []
+        in_dconv = BN_ReLU_DConv(in_channels_with_corr, out_channels * 2, 3)
+        shared_dconv_layers.append(in_dconv)
+
+        for i in range(5):
+            shared_dconv_layers.append(BN_ReLU_DConv(out_channels * 2, out_channels * 2, 3))
+
+        self.shared_dconv = torch.nn.Sequential(*shared_dconv_layers)
+
+        # F2M
+        self.f2m_head = BN_ReLU_DConv(out_channels * 2, input_frames * 2, 3)
+
+        # F2F
+        self.f2f_head = BN_ReLU_DConv(out_channels * 2, out_channels, 3)
+
+        # weights
+        self.weight_head = BN_ReLU_DConv(out_channels * 2, input_frames + 1, 3)
+
+        print(self.correlation, self.fusion, self.shared_dconv, self.f2m_head, self.f2f_head, self.weight_head)
+
+    def forward(self, input):
+        if self.do_normalization:
+            input = normalize_concatenated(input, self.mean, self.std)
+
+        B, C, H, W = input.shape
+        additional = dict()
+
+        fused = self.fusion(input)
+        corr = self.correlation(input)
+
+        combined = torch.cat((fused, corr), dim=1)
+        x = self.shared_dconv(combined)
+
+        flow = self.f2m_head(x)
+        predicted = self.f2f_head(x)
+        additional['predicted'] = predicted
+        weights = self.weight_head(x)
+        additional['weights'] = weights
+        probs = F.softmax(weights, dim=1)
+
+        warped = warp(input.reshape(B * 4, self.out_channels, H, W), flow.reshape(B * 4, 2, H, W))
+        warped = warped.reshape(B, self.in_channels, H, W)
+        additional['warped'] = warped
+
+        x = probs[:, 0:1, :, :] * predicted
+        for i in range(4):
+            x += probs[:, i + 1:i + 2, :, :] * warped[:, i * self.out_channels: (i + 1) * self.out_channels, :, :]
+
+        additional['output'] = x
+        return x, additional
+
+    def loss(self, additional, target, coef=(1.0, 0.5, 0.5)):
+        # compute f2m prediction
+        f2m_weights = additional['weights'][:, 1:, :, :]
+        probs = F.softmax(f2m_weights, dim=1)
+        f2m_out = torch.zeros_like(additional['output'], device=additional['output'].device)
+        for i in range(4):
+            f2m_out += probs[:, i:i + 1, :, :] * additional['warped'][:, i * self.out_channels: (i + 1) * self.out_channels, :, :]
+
+        loss_output = F.mse_loss(additional['output'], target)
+        loss_f2f = F.mse_loss(additional['predicted'], target)
+        loss_f2m = F.mse_loss(f2m_out, target)
+
+        return coef[0] * loss_output + coef[1] * loss_f2f + coef[2] * loss_f2m
+
+
+if __name__ == '__main__':
+
+    # just for testing
+    x = torch.cat(
+        [torch.ones(2, 4, 3, 3), torch.ones(2, 4, 3, 3) * 2, torch.ones(2, 4, 3, 3) * 3, torch.ones(2, 4, 3, 3) * 4],
+        dim=1)
+    w = torch.ones(2, 4, 3, 3)
+    w[:, 0, :, :] *= 0.1
+    w[:, 1, :, :] *= 0.2
+    w[:, 2, :, :] *= 0.3
+    w[:, 3, :, :] *= 0.4
+    print(x[:, 0, :, :] * w[:, 0, :, :])
+
+    F2MF(512, 128).cuda().forward(torch.randn(12, 512, 32, 64).cuda())
+
+    print(list(F2MF(512, 128).named_parameters()))
+
+    exit()
+
+    import numpy as np
+
+    p1 = '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/2_levels/1_skip_32x64/val/frankfurt_000000_001001_leftImg8bit.npy'
+    p2 = '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/2_levels/1_skip_32x64/val/frankfurt_000000_001007_leftImg8bit.npy'
+
+    x1 = torch.from_numpy(np.load(p1)).unsqueeze(0).cuda()
+    x2 = torch.from_numpy(np.load(p2)).unsqueeze(0).cuda()
+    # x1 = normalize(x1, x1.mean(), x1.std())
+    # x2 = normalize(x2, x2.mean(), x2.std())
+
+    corr_layer = SpatialCorrelationSampler(
+        kernel_size=1,
+        patch_size=9,
+        stride=1,
+        padding=0,
+        dilation=1,
+        dilation_patch=2)
+
+    x = torch.randn(12, 512, 32, 64)
+    B, C, H, W = x.shape
+    feats = [x[:, 128 * i: 128 * (i + 1), :, :] for i in range(4)]  # split by T
+    # conv_feats = [self.bn_relu_conv(x) for x in feats]
+    # norm_feats = [x / x.norm(dim=1, keepdim=True) for x in feats]
+
+    c_feats = torch.cat(feats, dim=1)
+    norm_feats = c_feats / c_feats.norm(dim=1, keepdim=True)
+    a = norm_feats.reshape(B * 4, 128, H, W)
+    b = norm_feats.reshape(B, C, H, W)
+
+    x = torch.randn(12, 512, 32, 64).cuda()
+    flow = torch.randn(12, 2, 32, 64).cuda()
+
+    a = torch.cat([warp(x[:, 128 * i: 128 * (i + 1), :, :], flow) for i in range(4)], dim=1)
+    b = warp(x.reshape(B * 4, 128, H, W), torch.cat([flow, flow, flow, flow], dim=1).reshape(B * 4, 2, H, W)).reshape(B,
+                                                                                                                      512,
+                                                                                                                      H,
+                                                                                                                      W)
+    print(a.shape)
+    print(b.shape)
+    print(torch.allclose(a, b))
+
+    c = warp(x[:, 0: 128, :, :], flow)
+    d = warp(x.reshape(B * 4, 128, H, W), torch.cat([flow, flow, flow, flow], dim=0))[:12, :, :, :]
+    print(c.shape)
+    print(d.shape)
+    print(torch.allclose(c, d))
+
+    print(a[3][0][0])
+    print(b[3][0][0])
+
+    [print(x.shape) for x in feats]
+    exit()
+
+    print(x1.shape)
+    xs = torch.cat((x1, x2, x1, x2), dim=1)
+    print(xs.shape)
+    # corr1 = CorrelationModule(in_channels_one=128).cuda()
+    # out1 = corr1.forward(xs)
+    # print(out1.shape)
+
+    corr1 = Correlation(pad_size=4 + int(3 // 2), kernel_size=3,
+                        max_displacement=4, stride1=1, stride2=1, corr_multiply=1)
+
+    correlation_sampler = SpatialCorrelationSampler(
+        kernel_size=1,
+        patch_size=9,
+        stride=1,
+        padding=0,
+        dilation=1,
+        dilation_patch=2)
+
+    from tqdm import tqdm
+
+    # for i in tqdm(range(10000)):
+    #     out1 = corr1(x1, x2)
+    #     out1[0,0,0,0] = 1
+    #
+    # for i in tqdm(range(10000)):
+    #     out2 = correlation_sampler(x1, x2)
+    #     out2[0, 0, 0, 0] = 1
+
+    for i in tqdm(range(100)):
+        out1 = corr1(x1, x2)
+
+    for i in tqdm(range(100)):
+        out2 = correlation_sampler(x1, x2)
+
+    print(out1.shape)
+    print(out2.shape)
+
+    # print(x2.shape)
+
+    # myCorr(x1, x2)
+
+    # kernel_size = 1
+    # max_displacement = 4
+    # corrLayer = Correlation(pad_size=max_displacement + int(kernel_size // 2), kernel_size=kernel_size,
+    #                         max_displacement=max_displacement, stride1=1, stride2=1, corr_multiply=1)
+
+    # out1 = corrLayer.forward(x1, x2)
+    #
+    # print(out1.shape)
+
+    # a = torch.randn(2, 3, 20, 30)
+    # b = torch.randn(2, 3, 20, 30)
+    # test_layer = CorrelationLayer(padding=4, kernel_size=1, max_displacement=4, stride_1=1, stride_2=2)
+    #
+    # print(test_layer(x1, x2))
+
+    out = out2.squeeze().cpu().reshape(81, 32, 64).numpy()
+
+    import matplotlib.pyplot as plt
+
+    fig, axs = plt.subplots(9, 9)
+    for i in range(9):
+        for j in range(9):
+            x = out[i * j + j, :, :]
+            axs[i, j].imshow(x)
+
+    plt.show()
---
+++ train_f2mf.py
@@ -0,0 +1,282 @@
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader, random_split
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+
+from PIL import Image
+from glob import glob
+from tqdm import tqdm
+import os
+import time
+import pickle
+
+from models.f2mf import *
+from models.semseg import SemsegModel
+from models.resnet.resnet_pyramid_forecast import *
+
+from data.cityscapes import *
+from evaluation.evaluate import ConfusionMatrix, compute_errors
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+moving_IDs = range(11, 18 + 1)
+timestep = 9
+f2f_levels = 8
+
+forecast_after_up_block = 2
+pyramid_levels = 3
+
+begin_time = time.strftime("%H_%M_%d_%m_%Y", time.localtime())
+custom_desc = '_64x32_shared_256'#'_128x64_first_3x3_rest_5x5'
+# current_dir = 'F2F-' + str(f2f_levels) + custom_desc + '_t' + str(timestep) + '_' + begin_time
+current_dir = 'F2MF-' + str(f2f_levels) + custom_desc + '_t' + str(timestep) + '_' + begin_time
+
+# SAVE_DIR = '/home/jakov/Desktop/swiftnet-forecasting/weights/F2F/DeformF2F-' + str(f2f_levels) + '/current/'
+SAVE_DIR = '/home/jakov/Desktop/swiftnet-forecasting/weights/test/' + str(pyramid_levels) + '_levels/' + current_dir + '/'
+os.mkdir(SAVE_DIR)
+print('Saving in: ' + SAVE_DIR)
+
+# mean = torch.tensor(
+#     np.load('/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features_pyr_forecast_2_skips/mean.npy'),
+#     device=device)
+# std = torch.tensor(
+#     np.load('/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features_pyr_forecast_2_skips/std.npy'),
+#     device=device)
+
+# f2f_model = DeformF2F_N(N=f2f_levels, in_channels=512, out_channels=128, mean=None, std=None)
+f2f_model = F2MF(in_channels=512, out_channels=128, mean=None, std=None)
+
+backbone = resnet18(pretrained=True,
+                    pyramid_levels=pyramid_levels,
+                    k_upsample=3,
+                    scale=1,
+                    mean=[73.15, 82.90, 72.3],
+                    std=[47.67, 48.49, 47.73],
+                    k_bneck=1,
+                    output_stride=4,
+                    efficient=True,
+                    forecast_after_up_block=forecast_after_up_block)
+
+semseg_model = SemsegModel(backbone, 19, k=1, bias=True)
+semseg_model.load_state_dict(
+    # torch.load('weights/rn18_pyramid/forecast/boundary/72-60_rn18_pyramid_forecast/stored/model_best.pt'), strict=False)
+    torch.load('weights/rn18_pyramid/test/3_levels/74-06_rn18_pyramid_forecast_2_skips/stored/model_best.pt'), strict=False)
+    # torch.load('weights/rn18_pyramid/test/3_levels/75-95_rn18_pyramid_forecast_3_skips/stored/model_best.pt'), strict=False)
+
+# Hyperparameters
+# learning_rate1 = 1e-3
+# learning_rate2 = 1e-4
+
+batch_size = 12
+
+num_epochs = 160
+num_epochs2 = 5
+
+
+# optimizer = torch.optim.Adam(f2f_model.parameters(), lr=learning_rate1)
+# optimizer1 = torch.optim.Adam(list(f2f_model.parameters()) + list(segm_model.parameters()), lr=learning_rate1)
+# optimizer2 = torch.optim.SGD(list(f2f_model.parameters()) + list(segm_model.parameters()), lr=learning_rate2)
+
+
+class SemsegCrossEntropy(nn.Module):
+    def __init__(self, num_classes=19, ignore_id=19, print_each=20):
+        super(SemsegCrossEntropy, self).__init__()
+        self.num_classes = num_classes
+        self.ignore_id = ignore_id
+        self.step_counter = 0
+        self.print_each = print_each
+
+    def loss(self, y, t):
+        if y.shape[2:4] != t.shape[1:3]:
+            y = upsample(y, t.shape[1:3])
+        return F.cross_entropy(y, target=t, ignore_index=self.ignore_id)
+
+    def forward(self, logits, labels, **kwargs):
+        # loss = logits.mean()
+        loss = self.loss(logits, labels)
+        # if (self.step_counter % self.print_each) == 0:
+        #    print(f'Step: {self.step_counter} Loss: {loss.data.cpu().item():.4f}')
+        self.step_counter += 1
+        return loss
+
+
+def evaluate(f2f_model, segm_model, loader):
+    f2f_model.eval()
+    segm_model.eval()
+
+    loss_fn_ce = SemsegCrossEntropy(ignore_id=255)
+
+    conf_matrix = ConfusionMatrix(num_classes=19, ignore_label=255)
+
+    loss_l2 = []
+    loss_ce = []
+
+    with torch.no_grad():
+        with tqdm(enumerate(loader), total=len(loader), desc='Testing') as progress:
+            for batch_idx, batch in progress:
+                past_feats, target_feats, sem_seg_gt = batch
+                past_feats = past_feats.to(device)
+
+                target_feats = target_feats.to(device)
+                sem_seg_gt = sem_seg_gt.to(device)
+
+                pred_feats, additional = f2f_model(past_feats)
+                # pred_feats = f2f_model.unnormalize(pred_feats)
+
+                loss = f2f_model.loss(additional, target_feats)
+                loss_l2.append(loss.cpu().item())
+
+                logits = segm_model.forward_decoder_no_skip(pred_feats, image_size=sem_seg_gt.shape[-2:])
+                preds = torch.argmax(logits, 1)
+                loss_ce.append(loss_fn_ce(logits, sem_seg_gt).cpu().item())
+                
+                conf_matrix.update(preds.cpu().numpy().flatten(), sem_seg_gt.cpu().numpy().flatten())
+
+        print("--- TEST ---")
+        # pixel_acc, iou_acc, recall, precision, _, per_class_iou = compute_errors(conf_matrix.get_matrix(), Cityscapes.class_info, verbose=True)
+        mse = np.mean(np.array(loss_l2))
+        ce = np.mean(np.array(loss_ce))
+        print("L2 (MSE) loss: ", mse)
+        print("Cross entropy loss: ", ce)
+        miou, per_class_iou = conf_matrix.get_metrics(verbose=False)
+        miou_mo = conf_matrix.get_subset_miou(moving_IDs)
+        print("mIoU: ", round(miou * 100, 2), '%')
+        print("mIoU-MO: ", round(miou_mo * 100, 2), '%')
+
+    f2f_model.train()
+    segm_model.train()
+    return miou, miou_mo, mse, ce
+
+
+if __name__ == '__main__':
+    dataset_train = CityscapesFeatureSequence(
+        # '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/2_levels/1_skip_32x64/train',
+        '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/3_levels/2_skips_32x64/train',
+        # '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/3_levels/3_skips_64x128/train',
+        # '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/3_levels/new/3_skips_64x128/train',
+        '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/gtFine/train',
+        delta=timestep, subset='train')
+    dataset_val = CityscapesFeatureSequence(
+        # '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/2_levels/1_skip_32x64/val',
+        '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/3_levels/2_skips_32x64/val',
+        # '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/3_levels/3_skips_64x128/val',
+        # '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/features/3_levels/new/3_skips_64x128/val',
+        '/run/media/jakov/2TB KC3000/Users/bubas/Data/Cityscapes/gtFine/val',
+        delta=timestep, subset='val')
+
+    train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, collate_fn=None, num_workers=2,
+                              persistent_workers=True, prefetch_factor=2)
+    val_loader = DataLoader(dataset_val, batch_size=1, shuffle=False, collate_fn=None, num_workers=2,
+                            persistent_workers=True, prefetch_factor=4)
+
+    metrics_dict = {
+        'val_miou': [],
+        'val_miou_mo': [],
+        'val_mse': [],
+        'val_ce': [],
+        'train_miou': [],
+        'train_miou_mo': [],
+        'train_mse': [],
+        'train_ce': []
+    }
+
+    # input_features = 128
+    # num_classes = 19
+    # output_features_res = (128, 256)
+    # output_preds_res = (512, 1024)
+    # mean = torch.tensor(np.load("../cityscapes_halfres_features_r18/mean.npy"), requires_grad=False, device=device).view(1, input_features, 1, 1)
+    # std = torch.tensor(np.load("../cityscapes_halfres_features_r18/std.npy"), requires_grad=False, device=device).view(1, input_features, 1, 1)
+
+    # f2f_model.load_state_dict(torch.load("./f2f_model.pth", map_location = device))
+    f2f_model.to(device)
+    semseg_model.to(device)
+    # print(f2f_model)
+    f2f_model.train()
+    # semseg_model.train()
+
+    loss_fn_ce = SemsegCrossEntropy(ignore_id=255)
+    optimizer = torch.optim.Adam(f2f_model.parameters(), lr=5e-4)
+    # optimizer = torch.optim.Adam(list(f2f_model.parameters()) + list(semseg_model.parameters()), lr=learning_rate1)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader), eta_min=1e-7)
+
+    conf_matrix = ConfusionMatrix(num_classes=19, ignore_label=255)
+
+    max_val_miou = 0
+    max_val_miou_mo = 0
+
+    for epoch in range(1, num_epochs + 1):
+        loss_l2 = []
+        loss_ce = []
+
+        # print('LR:', scheduler.get_last_lr()[0])
+
+        with tqdm(enumerate(train_loader), total=len(train_loader),
+                  desc=f'Training (epoch={epoch}/{num_epochs})') as epoch_progress:
+            for batch_idx, batch in epoch_progress:
+                past_feats, target_feats, sem_seg_gt = batch
+                past_feats = past_feats.to(device)
+                target_feats = target_feats.to(device)
+                # target_feats_normalized = f2f_model.normalize(target_feats)
+                # sem_seg_gt = sem_seg_gt.to(device)
+                # print(past_feats.shape, target_feats.shape)
+
+                optimizer.zero_grad()
+
+                pred_feats, additional = f2f_model(past_feats)
+
+                loss = f2f_model.loss(additional, target_feats)
+                loss_l2.append(loss.cpu().item())
+
+                # logits = semseg_model.forward_decoder_no_skip(pred_feats, image_size=sem_seg_gt.shape[1:])
+                # preds = torch.argmax(logits, 1)
+                # semseg_loss = loss_fn_ce(logits, sem_seg_gt)
+                # loss = loss + semseg_loss
+                # loss_ce.append(semseg_loss.cpu().item())
+
+                # conf_matrix.update(preds.cpu().numpy().flatten(), sem_seg_gt.cpu().numpy().flatten())
+
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+
+        # scheduler.step()
+
+        print("--- TRAIN ---")
+        train_mse = np.mean(np.array(loss_l2))
+        # train_ce = np.mean(np.array(loss_ce))
+        print("L2 (MSE) loss: ", train_mse)
+        # print("Cross entropy loss: ", train_ce)
+        # train_miou, train_per_class_iou = conf_matrix.get_metrics(verbose=False)
+        # train_miou_mo = conf_matrix.get_subset_miou(moving_IDs)
+        # print("mIoU: ",  round(train_miou * 100, 2), '%')
+        # print("mIoU-MO: ", round(train_miou_mo * 100, 2), '%')
+
+        # conf_matrix.reset()
+
+        val_miou, val_miou_mo, val_mse, val_ce = evaluate(f2f_model, semseg_model, val_loader)
+        metrics_dict['val_miou'].append(val_miou)
+        metrics_dict['val_miou_mo'].append(val_miou_mo)
+        metrics_dict['val_mse'].append(val_mse)
+        metrics_dict['val_ce'].append(val_ce)
+        metrics_dict['train_mse'].append(train_mse)
+        # metrics_dict['train_ce'].append(train_ce)
+        # metrics_dict['train_miou'].append(train_miou)
+        # metrics_dict['train_miou_mo'].append(train_miou_mo)
+
+        if val_miou > max_val_miou:
+            max_val_miou = val_miou
+            max_val_miou_mo = val_miou_mo
+            f2f_model.eval()
+            torch.save(f2f_model.state_dict(), SAVE_DIR + 'f2f_model.pth')
+            f2f_model.train()
+            # torch.save(semseg_model.state_dict(), SAVE_DIR + 'semseg_model.pth')
+            print('Saving')
+
+        with open(SAVE_DIR + 'metrics.pickle', 'wb') as f:
+            pickle.dump(metrics_dict, f)
+        print()
+
+    os.rename(SAVE_DIR, SAVE_DIR.replace(current_dir, current_dir + '_' + str(round(max_val_miou * 100, 2)) + '_' + str(
+        round(max_val_miou_mo * 100, 2))))
